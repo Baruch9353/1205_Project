332e3e764a76afbac7abd9031922e8b75d59457a
baruch <baruchminzberg@gmail.com>
2025-05-12 17:04:46 +0300

    Add project files.

---
+++ 1205_Project/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1205_Project
+{
+    internal class Program
+    {
+
+        //List of numbers
+        List<double> numbers = new List<double>();
+
+        //args check
+
+
+
+
+
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
